7f6aa818e337452c8170f61de4c5e0773f7020ce
Ravikant Gupta <ravikant0909@gmail.com>
2015-03-21 23:37:18 -0400

    All get queries done
    • Add categorization. All listings are now automatically categorized
    • The rest queries now return entries sorted by creation date
    • The date field now contains post creation date time
    • You can get all listings for a particular user or a category
    • The updated rest call now has the capacity to update the whole of GT
    Thrift entries in our database

--- ServerCode/gt-thrift/main.py
+++ ServerCode/gt-thrift/main.py
@@ -17,11 +17,24 @@
 import webapp2
 import urllib2
 import json
+import logging
+
 from google.appengine.ext import ndb
+
+from constants import CATEGORIES, PER_PAGE, CATEGORY_SMALL_TO_BIG_MAP
 
 START_URL = "https://graph.facebook.com/v2.2/199456403537988/feed?access_token=CAANI8ZBCAeOwBAHy59OJRb3B9y9X9aNn2XekTCjBsEWlR4H71M0srnqT4DJ6fnZBvPfilH8Nm4xXBYAZA3JwMqD5ElrlcptoNaEDZCRYtRTaPqhRqio7S6ioxTYXOT2ABANENj2mrHF6FNBv6et3X7hrbMzsIU8ZBK3YN5LlcNzAoQFpBB3HtNw354QdVggfLAemF4fzElS3nVRB6yOqE"
 DEFAULT_GUESTBOOK_NAME = 'default_guest'
-per_page = 20
+
+
+def categorize(description):
+    if description:
+        words = description.split()
+        for category_name, keyword_dict in CATEGORIES:
+            for word in words:
+                if word.lower() in keyword_dict:
+                    return category_name
+    return "Others"
 
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
     """Constructs a Datastore key for a Guestbook entity.
@@ -33,164 +46,153 @@ class Listing(ndb.Model):
     title = ndb.StringProperty(indexed=True)
     message = ndb.StringProperty(indexed=True)
     category = ndb.StringProperty(indexed=True)
-    date = ndb.DateTimeProperty(auto_now_add=True)
+    date = ndb.StringProperty(indexed=True)
     picture = ndb.StringProperty(indexed=False)
     post_id = ndb.StringProperty(indexed=True)
     author_name = ndb.StringProperty(indexed=False)
     author_id = ndb.StringProperty(indexed=True)
     link_to_post = ndb.StringProperty(indexed=False)
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        url = START_URL
-        json_response = urllib2.urlopen(url)
-        data = json.load(json_response)
-        listings_list = []
-        all_listings = Listing.query()
-        cnt = 0
-        # for each in all_listings:
-        #     cnt += 1
-        # self.response.write("query count: " + str(all_listings.count()))
-        # self.response.write(str(all_listings))
-        # self.response.write(data['data'][0])
-        for item in data['data']:
-            new_listing = Listing(parent=guestbook_key(DEFAULT_GUESTBOOK_NAME))
-            d = {}
-            if 'message' in item:
-                messagez = item['message']
-            else:
-                messagez = ""
-            title = " ".join(messagez.split()[:10]) + " ..."
-            d['message'] = messagez
-            d['title'] = title
-
-            if 'picture' in item:
-                d['picture'] = item['picture']
-
-            res = Listing.query(Listing.post_id == item['id'])
-            if res.count() > 0:
-                cnt += 1
-            # new_listing.title = d['title']
-            # new_listing.message = d['message']
-            # new_listing.post_id = item['id']
-            # new_listing.author_id = item['from']['id']
-            # new_listing.author_name = item['from']['name']
-            # new_listing.category = "all"
-            # new_listing.put()
-            listings_list.append(d)
-        js = json.dumps(listings_list)
-        self.response.write(str(cnt))
-        
-
-class PageHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write("hiiiii")
 
 class DeleteAllHandler(webapp2.RequestHandler):
     def get(self):
         ndb.delete_multi(Listing.query().fetch(keys_only=True))
         self.response.write("Deleted all keys")
-
-class PageCountHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write(str(Listing.query().count()))
         
 class updatedbHandler(webapp2.RequestHandler):
+    """
+    This function take all the history from Facebook group and update it in our database
+    We need not run this 
+    """
     def get(self):
         url = START_URL
-        json_response = urllib2.urlopen(url)
-        data = json.load(json_response)
         cnt = 0
-        for item in data['data']:
-            res = Listing.query(Listing.post_id == item['id'])
-            if res.count() == 0:
-                d = {}
-                if 'message' in item:
-                    messagez = item['message']
-                else:
-                    messagez = ""
-                title = " ".join(messagez.split()[:10]) + " ..."
-                d['message'] = messagez
-                d['title'] = title
+        # debug_string = ""
+        for page_no in range(20):
+            json_response = urllib2.urlopen(url)
+            data = json.load(json_response)
+            # debug_string += "Page no fetching is " + str(page_no) + "\n"
+            for item in data['data']:
+                res = Listing.query(Listing.post_id == item['id'])
+                if res.count() == 0:
+                    d = {}
+                    if 'message' in item:
+                        messagez = item['message']
+                    else:
+                        messagez = ""
+                    title = " ".join(messagez.split()[:10]) + " ..."
+                    d['message'] = messagez
+                    d['title'] = title
 
-                if 'picture' in item:
-                    d['picture'] = item['picture']
-                else:
-                    d['picture'] = ""
-                new_listing = Listing(parent=guestbook_key(DEFAULT_GUESTBOOK_NAME))
-                new_listing.title = d['title']
-                new_listing.message = d['message'][0:500]
-                new_listing.post_id = item['id']
-                new_listing.author_id = item['from']['id']
-                new_listing.author_name = item['from']['name']
-                new_listing.category = "all"
-                new_listing.picture = d['picture']
-                if 'link' in item:
-                    new_listing.link_to_post = item['link']
-                new_listing.put()
-                cnt += 1
-        self.response.write("Added Items: " + str(cnt))
+                    if 'picture' in item:
+                        d['picture'] = item['picture']
+                    else:
+                        d['picture'] = ""
+                    new_listing = Listing(parent=guestbook_key(DEFAULT_GUESTBOOK_NAME))
+                    new_listing.title = d['title']
+                    new_listing.message = d['message'][0:500]
+                    new_listing.post_id = item['id']
+                    new_listing.date = item['created_time']
+                    new_listing.author_id = item['from']['id']
+                    new_listing.author_name = item['from']['name']
+                    # debug_string += "Messages : " + d['message'] + "\n"
+                    # debug_string += "Category : " + categorize(d['message']) + "\n"
+                    new_listing.category = categorize(d['message'])
+                    new_listing.picture = d['picture']
+                    if 'link' in item:
+                        new_listing.link_to_post = item['link']
+                    new_listing.put()
+                    cnt += 1
+            url = data['paging']['next']
+        self.response.write("Updated the database with " + str(cnt) + " new entries")
 
-class UserHandler(webapp2.RequestHandler):
-    def get(self, username):
-        self.response.write("Requested records for user: " + str(username))
 
-class getPostsHandler(webapp2.RequestHandler):
-    def get(self):
-        result = Listing.query()
-        # self.response.write(result.count())
+class CategoryHandler(webapp2.RequestHandler):
+    def get(self, cat, page_no):
+        category = str(cat)
+        page_no = int(page_no)
+        category_big = CATEGORY_SMALL_TO_BIG_MAP[category]
+        logging.debug("Category received is " + category_big)
+        result = Listing.query(Listing.category == category_big).order(-Listing.date);
         listings_list = []
-        page_no = 1
         cnt = 0
+        cont_count = 0
+        # debug_string = []
         for listing in result:
             cnt = cnt + 1
-            if cnt > (per_page * page_no - 1) and cnt < (per_page * page_no):
-                continue
-            d = {}
-            d['message'] = listing.message
-            d['title'] = listing.title
-            d['post_id'] = listing.post_id
-            d['author_id'] = listing.author_id
-            d['author_name'] = listing.author_name
-            d['category'] = listing.category
-            d['picture'] = listing.picture
-            d['link_to_post'] = listing.link_to_post
-            listings_list.append(d)
+            if cnt > (PER_PAGE * (page_no - 1)) and cnt <= (PER_PAGE * page_no):
+                d = {}
+                d['message'] = listing.message
+                d['title'] = listing.title
+                d['post_id'] = listing.post_id
+                d['author_id'] = listing.author_id
+                d['author_name'] = listing.author_name
+                d['category'] = listing.category
+                d['picture'] = listing.picture
+                d['date'] = str(listing.date)
+                d['link_to_post'] = listing.link_to_post
+                listings_list.append(d)
         js = json.dumps(listings_list)
         self.response.write(js)
 
-class getSecondPageHander(webapp2.RequestHandler):
-    def get(self):
-        result = Listing.query()
-        # self.response.write(result.count())
+
+class UserHandler(webapp2.RequestHandler):
+    def get(self, author_id, page_no):
+        category = str(author_id)
+        page_no = int(page_no)
+        result = Listing.query(Listing.author_id == author_id).order(-Listing.date);
         listings_list = []
-        page_no = 2
         cnt = 0
+        cont_count = 0
+        # debug_string = []
         for listing in result:
-            d = {}
             cnt = cnt + 1
-            if cnt > (per_page * page_no - 1) and cnt < (per_page * page_no):
-                continue
-            d['message'] = listing.message
-            d['title'] = listing.title
-            d['post_id'] = listing.post_id
-            d['author_id'] = listing.author_id
-            d['author_name'] = listing.author_name
-            d['category'] = listing.category
-            d['picture'] = listing.picture
-            d['link_to_post'] = listing.link_to_post
-            listings_list.append(d)
+            if cnt > (PER_PAGE * (page_no - 1)) and cnt <= (PER_PAGE * page_no):
+                d = {}
+                d['message'] = listing.message
+                d['title'] = listing.title
+                d['post_id'] = listing.post_id
+                d['author_id'] = listing.author_id
+                d['author_name'] = listing.author_name
+                d['category'] = listing.category
+                d['picture'] = listing.picture
+                d['date'] = str(listing.date)
+                d['link_to_post'] = listing.link_to_post
+                listings_list.append(d)
+        js = json.dumps(listings_list)
+        self.response.write(js)
+
+
+class PageHandler(webapp2.RequestHandler):
+    def get(self, page_no):
+        page_no = int(page_no)
+        logging.debug("Page no received is " + str(page_no))
+        result = Listing.query().order(-Listing.date);
+        listings_list = []
+        cnt = 0
+        cont_count = 0
+        # debug_string = []
+        for listing in result:
+            cnt = cnt + 1
+            if cnt > (PER_PAGE * (page_no - 1)) and cnt <= (PER_PAGE * page_no):
+                d = {}
+                d['message'] = listing.message
+                d['title'] = listing.title
+                d['post_id'] = listing.post_id
+                d['author_id'] = listing.author_id
+                d['author_name'] = listing.author_name
+                d['category'] = listing.category
+                d['picture'] = listing.picture
+                d['date'] = str(listing.date)
+                d['link_to_post'] = listing.link_to_post
+                listings_list.append(d)
         js = json.dumps(listings_list)
         self.response.write(js)
 
 app = webapp2.WSGIApplication([
-    ('/getfirstpage/?',getPostsHandler),
-    ('/getsecondpage/?',getSecondPageHander),
-    ('/getuser/<username:\s+>',UserHandler),
-    ('/getpagecount/?',PageCountHandler),
+    (r'/getlistings/(\d+)', PageHandler),
+    (r'/getlistings/user/(.*)/(\d+)', UserHandler),
+    (r'/getlistings/(.*)/(\d+)', CategoryHandler),
     ('/deleteall/?',DeleteAllHandler),
-    ('/page/?',PageHandler),
-    ('/updatedb/?',updatedbHandler),
-    ('/', MainHandler)
+    ('/updatedb/?',updatedbHandler)
 ], debug=True)
